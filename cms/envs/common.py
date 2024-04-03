5b137917c8f40d677e7b74f64f943c2ae7d4af19
Jesper Hodge <jhodge@outlook.de>
2024-04-03 19:33:35 -0400

    fix: refactor role access cache to use dict data structure for O(1) complexity for checks

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -970,6 +970,7 @@ MIDDLEWARE = [
 
     # This must be last so that it runs first in the process_response chain
     'openedx.core.djangoapps.site_configuration.middleware.SessionCookieDomainOverrideMiddleware',
+    'django_cprofile_middleware.middleware.ProfilerMiddleware'
 ]
 
 EXTRA_MIDDLEWARE_CLASSES = []
@@ -1190,7 +1191,7 @@ CODE_JAIL_REST_SERVICE_READ_TIMEOUT = 3.5  # time in seconds
 
 ############################ DJANGO_BUILTINS ################################
 # Change DEBUG in your environment settings files, not here
-DEBUG = False
+DEBUG = True
 SESSION_COOKIE_SECURE = False
 SESSION_SAVE_EVERY_REQUEST = False
 SESSION_SERIALIZER = 'openedx.core.lib.session_serializers.PickleSerializer'
@@ -1539,6 +1540,9 @@ CELERY_QUEUES = {
     DEFAULT_PRIORITY_QUEUE: {},
     LOW_PRIORITY_QUEUE: {},
 }
+
+DJANGO_CPROFILE_MIDDLEWARE_REQUIRE_STAFF = False
+
 
 # Queues configuration
 
